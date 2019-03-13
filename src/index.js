97810d3128f727c731e0e983774eba0e232b8347
DmitriyShadziun <m_e_x_a_n_i_k@tut.by>
2019-03-13 12:30:25 +0300

    docs

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+   var length = preferences.length; 
+  var count = 0;  
+  for (var i = 0; i<length; i++) { 
+      var Love1 = preferences[i] - 1;
+        console.log(Love1);
+      if (Love1 == i) continue;
+      var Love2 = preferences[Love1] - 1;
+      if (Love1 == Love2) continue;
+      var Love3 = preferences[Love2] - 1;
+      if (Love3 == Love2) continue;
+      if (Love3 == i) {
+          count++;
+      }
+   }
+   return count / 3;
 };
